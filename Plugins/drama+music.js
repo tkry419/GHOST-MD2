ae0b5938028d1c45884fb338de19183a53ee9cb5
tkry419 <chukwudiasika16@gmail.com>
2026-06-10 17:55:03 +0100

    Create drama+music.js

---
+++ Plugins/drama+music.js
@@ -0,0 +1,105 @@
+const {cmd , commands} = require('../command')
+const fg = require('api-dylux')
+const yts = require('yt-search')
+cmd({
+    pattern: "play2",
+    desc: "To download songs.",
+    react: "🎵",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("Please give me a url or title")  
+const search = await yts(q)
+const data = search.videos[0];
+const url = data.url
+    
+    
+let desc = `
+*⦁ MUSⵊC DOWNLOADⵊNG ⦁*
+
+🎵 *MUSⵊC FOUND!* 
+
+➥ *Title:* ${data.title} 
+➥ *Duration:* ${data.timestamp} 
+➥ *Views:* ${data.views} 
+➥ *Uploaded On:* ${data.ago} 
+➥ *Link:* ${data.url} 
+
+🎧 *ENJOY THE MUSIC BROUGHT TO YOU!*
+
+
+> *© Powered By GHOST-MD* 
+`
+
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
+
+//download audio
+
+let down = await fg.yta(url)
+let downloadUrl = down.dl_url
+
+//send audio message
+await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
+await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"audio/mpeg",fileName:data.title + ".mp3",caption:"*© Powered By GHOST-MD*"},{quoted:mek})
+
+}catch(e){
+console.log(e)
+  reply(`_Hi ${pushname} retry later_`)
+}
+})
+
+//====================video_dl=======================
+
+cmd({
+    pattern: "darama",
+    alias: ["video2"],
+    desc: "To download videos.",
+    react: "🎥",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("Please give me a url or title")  
+const search = await yts(q)
+const data = search.videos[0];
+const url = data.url
+    
+    
+let desc = `
+* ⦁ VⵊDEO DOWNLOADⵊNG ⦁ *
+
+🎥 *VⵊDEO FOUND!* 
+
+➥ *Title:* ${data.title} 
+➥ *Duration:* ${data.timestamp} 
+➥ *Views:* ${data.views} 
+➥ *Uploaded On:* ${data.ago} 
+➥ *Link:* ${data.url} 
+
+🎬 *ENJOY THE VIDEO BROUGHT TO YOU!*
+
+
+> *© Powered By GHOST-MD*
+`
+
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
+
+//download video
+
+let down = await fg.ytv(url)
+let downloadUrl = down.dl_url
+
+//send video message
+await conn.sendMessage(from,{video: {url:downloadUrl},mimetype:"video/mp4"},{quoted:mek})
+await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"video/mp4",fileName:data.title + ".mp4",caption:"*© Powered By GHOST-MD*"},{quoted:mek})
+
+}catch(e){
+console.log(e)
+  reply(`_Hi ${pushname} retry later_`)
+}
+})
+//
+  
