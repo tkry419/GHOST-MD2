1c46005a05c86a8bed7e175d269d249ed932ca41
tkry419 <chukwudiasika16@gmail.com>
2026-06-10 17:00:02 +0100

    Create menu2.js

---
+++ Plugins/menu2.js
@@ -0,0 +1,98 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+cmd({
+    pattern: "menu2",
+    react: "👾",
+    desc: "get cmd list",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let menu = {
+main: '',
+download: '',
+group: '',
+owner: '',
+convert: '',
+search: ''
+};
+
+for (let i = 0; i < commands.length; i++) {
+if (commands[i].pattern && !commands[i].dontAddCommandList) {
+menu[commands[i].category] += `*┋* ${commands[i].pattern}\n`;
+ }
+}
+
+let madeMenu = `*╭─────────────────❒⁠⁠⁠⁠*
+
+*⇆ ʜɪ ᴍʏ ᴅᴇᴀʀ ғʀɪᴇɴᴅ ⇆*
+
+     *${pushname}*
+
+┏━━━━━━━━━━━━━━━━━━━━
+   *Welcome to GHOST-MD 2nd Menu*
+┗━━━━━━━━━━━━━━━━━━━━
+
+*Created by GHOST👨🏻‍💻*
+
+
+*╭───────────────❒⁠⁠⁠⁠*
+*│* *❂ DOWNLOAD CMDS❂*
+*┕───────────────❒*
+*╭──────────●●►*
+${menu.download}
+*╰──────────●●►*
+
+*╭───────────────❒⁠⁠⁠⁠*
+*│* *❂MAIN CMDS❂*
+*┕───────────────❒*
+*╭──────────●●►*
+${menu.main}
+*╰──────────●●►*
+
+*╭───────────────❒⁠⁠⁠⁠*
+*│* *❂GROUP CMDS❂*
+*┕───────────────❒*
+
+*╭──────────●●►*
+${menu.group}
+*╰──────────●●►*
+
+*╭───────────────❒⁠⁠⁠⁠*
+*│* *❂OWNER CMDS❂*
+*┕───────────────❒*
+
+*╭──────────●●►*
+${menu.owner}
+*╰──────────●●►*
+
+*╭───────────────❒⁠⁠⁠⁠*
+*│* *❂CONVERTER CMDS❂*
+*┕───────────────❒*
+
+*╭──────────●●►*
+${menu.convert}
+*╰──────────●●►*
+
+*╭─────────────────❒⁠⁠⁠⁠*
+*│* *❂SEARCH CMDS ❂*
+*┕─────────────────❒*
+
+*╭──────────●●►*
+${menu.search}
+*╰──────────●●►*
+
+
+> *Powered by GHOST-MD*
+
+`;
+
+await conn.sendMessage(from,{image:{url:config.MENU_IMG},caption:madeMenu},{quoted:mek})
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
+                                    
